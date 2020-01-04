eb73fcc2d4a90c23f5cd7a083fbe3b1d2da02fbf
Eduardo Perez <edujpp1@gmail.com>
2020-01-04 14:44:03 -0300

    isomorphic-unfetch was added for make the queries to the CoinMarketCap API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import MasterPage from './components/Master';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => (
+const Index = (props) => (
   <MasterPage>
-    <div>
-      <p>Hola mundo!</p>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h2>Precio del Bitcoin</h2>
+          { console.log(props) }
+        </div>
+        <div className="col-8">
+          <h2>Noticias  sobre Bitcoin</h2>
+        </div>
+        <div className="col-4">
+          <h2>Próximos eventos sobre Bitcoin</h2>
+        </div>
+      </div>
     </div>
   </MasterPage>
-);
+)
+
+// Once the component is loaded, it make the query to the API and save it like props
+Index.getInitialProps = async () => {
+  const precio = await fetch('https://api.coinmarketcap.com/v2/ticker/1/');
+  const resPrecio = await precio.json();  
+  return {
+    precioBitcoin: resPrecio.data.quotes.USD
+  }
+}
 
 export default Index;
