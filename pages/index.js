13ea802072748ddbee35f84ff767e85a28037b86
Eduardo Perez <edujpp1@gmail.com>
2020-01-04 15:14:28 -0300

    The call to the newsAPI was made it

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import MasterPage from './components/Master';
 import Precio from './components/Precio';
+import Noticias from './components/Noticias';
 import fetch from 'isomorphic-unfetch';
 
 const Index = (props) => (
@@ -8,12 +9,15 @@ const Index = (props) => (
       <div className="row">
         <div className="col-12">
           <h2>Precio del Bitcoin</h2>
-            <Precio
-              precio={props.precioBitcoin}
-            />
+          <Precio
+            precio={props.precioBitcoin}
+          />
         </div>
         <div className="col-8">
           <h2>Noticias  sobre Bitcoin</h2>
+          <Noticias 
+            noticias={props.noticias}
+          />
         </div>
         <div className="col-4">
           <h2>Próximos eventos sobre Bitcoin</h2>
@@ -26,9 +30,14 @@ const Index = (props) => (
 // Once the component is loaded, it make the query to the API and save it like props
 Index.getInitialProps = async () => {
   const precio = await fetch('https://api.coinmarketcap.com/v2/ticker/1/');
+  const noticias = await fetch('https://newsapi.org/v2/everything?q=bitcoin&from=2019-12-04&sortBy=publishedAt&apiKey=64894da34df448a5af3268a7a15778ea&language=es');
+
   const resPrecio = await precio.json();  
+  const resNoticias = await noticias.json();
+
   return {
-    precioBitcoin: resPrecio.data.quotes.USD
+    precioBitcoin: resPrecio.data.quotes.USD,
+    noticias: resNoticias.articles
   }
 }
 
